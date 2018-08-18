7377adedc259b0fff39ea4c7cf909cb7a6a6bf34
EmeraldTurtle <Emeraldturtle@MacbookSlow>
2018-08-18 11:31:15 +0700

    fixed the screen movement animating thing

--- scripts/AnimationManager.js
+++ scripts/AnimationManager.js
@@ -5,50 +5,55 @@ class Animate {
         this.bottom_to_be = null;
         this.interval = null;
         this.speed = null;
+        this.isAnimating = false;
     }
     translate(dx, dy, fps = 60, speed = 32) {
-        this.speed = speed;
-        this.left_to_be = this.object.left + dx;
-        this.bottom_to_be = this.object.bottom + dy;
-        var self = this;
-        this.interval = window.setInterval(function () {
-            dx = self.left_to_be - self.object.left;
-            dy = self.bottom_to_be - self.object.bottom;
-            if (Math.abs(self.object.left - self.left_to_be +
-                self.object.bottom - self.bottom_to_be) < 0.01) {
-                clearInterval(self.interval);
-                self.object.setPosition(self.left_to_be, self.bottom_to_be);
-            }
-            else {
-                if (dx > 0) {
-                    self.object.setPosition(self.object.left + self.speed, self.object.bottom);
-                    //if the object surpass the destination
-                    if (self.object.left > self.left_to_be) {
-                        self.object.setPosition(self.left_to_be, self.bottom_to_be);
+        if (!this.isAnimating) {
+            this.isAnimating = true;
+            this.speed = speed;
+            this.left_to_be = this.object.left + dx;
+            this.bottom_to_be = this.object.bottom + dy;
+            var self = this;
+            this.interval = window.setInterval(function () {
+                dx = self.left_to_be - self.object.left;
+                dy = self.bottom_to_be - self.object.bottom;
+                if (Math.abs(self.object.left - self.left_to_be +
+                    self.object.bottom - self.bottom_to_be) < 0.01) {
+                    clearInterval(self.interval);
+                    self.object.setPosition(self.left_to_be, self.bottom_to_be);
+                    self.isAnimating = false;
+                }
+                else {
+                    if (dx > 0) {
+                        self.object.setPosition(self.object.left + self.speed, self.object.bottom);
+                        //if the object surpass the destination
+                        if (self.object.left > self.left_to_be) {
+                            self.object.setPosition(self.left_to_be, self.bottom_to_be);
+                        }
+                    }
+                    else if (dx < 0) {
+                        self.object.setPosition(self.object.left - self.speed, self.object.bottom);
+                        //if the object surpass the destination
+                        if (self.object.left < self.left_to_be) {
+                            self.object.setPosition(self.left_to_be, self.object.bottom);
+                        }
+                    }
+                    if (dy > 0) {
+                        self.object.setPosition(self.object.left, self.object.bottom + self.speed);
+                        //if the object surpass the destination
+                        if (self.object.bottom > self.bottom_to_be) {
+                            self.object.setPosition(self.object.left, self.bottom_to_be);
+                        }
+                    }
+                    else if (dy < 0) {
+                        self.object.setPosition(self.object.left, self.object.bottom - self.speed);
+                        //if the object surpass the destination
+                        if (self.object.bottom < self.bottom_to_be) {
+                            self.object.setPosition(self.object.left, self.bottom_to_be);
+                        }
                     }
                 }
-                else if (dx < 0) {
-                    self.object.setPosition(self.object.left - self.speed, self.object.bottom);
-                    //if the object surpass the destination
-                    if (self.object.left < self.left_to_be) {
-                        self.object.setPosition(self.left_to_be, self.object.bottom);
-                    }
-                }
-                if (dy > 0) {
-                    self.object.setPosition(self.object.left, self.object.bottom + self.speed);
-                    //if the object surpass the destination
-                    if (self.object.bottom > self.bottom_to_be) {
-                        self.object.setPosition(self.object.left, self.bottom_to_be);
-                    }
-                }
-                else if (dy < 0) {
-                    self.object.setPosition(self.object.left, self.object.bottom - self.speed);
-                    //if the object surpass the destination
-                    if (self.object.bottom < self.bottom_to_be) {
-                        self.object.setPosition(self.object.left, self.bottom_to_be);
-                    }
-                }
-            }
-        }, 1000 / fps);
+            }, 1000 / fps);
+        }
     };
 }
